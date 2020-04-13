5c890e8136eced44328517529e1f1c651970c0b3
Dr-Aculass <heroufosse.martin@gmail.com>
2020-04-13 18:40:58 +0200

    Test commit & push

--- Application/src/Adresse.java
+++ Application/src/Adresse.java
@@ -5,5 +5,5 @@ public class Adresse {
         // Comparaison sur le code postal (basé sur la province) https://fr.wikipedia.org/wiki/Liste_des_codes_postaux_belges ?
         // Sinon : https://github.com/googlemaps/google-maps-services-java ?
         return d;                
-    } //test commit
+    } //test commit & push
 }
