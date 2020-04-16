b596188eeb4e63e937c9841c414c3e6eaf1c8691
Benzzzai <44294294+Benzzzai@users.noreply.github.com>
2020-04-16 17:48:14 +0200

    Add files via upload

---
+++ Adresse.java
@@ -0,0 +1,9 @@
+public class Adresse { 
+    public int distance (Adresse a, Adresse b){
+        int d = 0;
+        // calcul ?
+        // Comparaison sur le code postal (basée sur la province) https://fr.wikipedia.org/wiki/Liste_des_codes_postaux_belges ?
+        // Sinon : https://github.com/googlemaps/google-maps-services-java ?
+        return d;                
+    }
+}
---
+++ Patient.java
@@ -0,0 +1,21 @@
+public class Patient {
+    //public int key;
+    private int typebesoin;
+    private Adresse adresse; // code postal
+    private String nom; // + info perso (âge, sexe)
+    // + Numéro national (NISS)
+    
+    public Patient(String nom, int typebesoin, Adresse adresse){
+        this.nom = nom;
+        this.typebesoin = typebesoin;
+        this.adresse = adresse; 
+    }
+    
+    public int gettype(){
+        return typebesoin;
+    }
+    
+    public Adresse getadresse(){
+        return adresse;
+    }    
+}
